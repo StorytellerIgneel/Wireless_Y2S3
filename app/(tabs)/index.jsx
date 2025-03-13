9eea2fa4f72b0afb9e239fea83972033f0d8ae50
LIANG JING HOE <liang.jinghoe@1utar.my>
2025-03-13 16:00:30 +0800

    simple notification system

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -3,11 +3,11 @@ import { View, StyleSheet, Pressable, Text } from "react-native";
 import ChatScreen from "../chat";
 import FeedbackScreen from "../feedback";
 import { Link } from "expo-router";
-import Login from "../login";
+import Notif from "../notification";
 
 const App = () => {
   return (
-    <Login />
+    <Notif />
   )
 };
 
